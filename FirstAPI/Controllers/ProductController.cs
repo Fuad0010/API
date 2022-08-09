fd88a33c2bcc9bf44a42ca28599b35369e4bbf38
Fuad0010 <fuadft@code.edu.az>
2022-08-09 17:39:13 +0400

    feat:showing database in browser url

---
+++ FirstAPI/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using FirstAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public static List<Product> products =
+            new List<Product>
+            {
+                new Product { Id = 1, Name = "Tozsoran", Price = 999.99 },
+              { new Product { Id = 2, Name = "Fen", Price = 24.99 } }
+
+            };
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetOne(int id)
+        {
+            Product p=products.FirstOrDefault(p=>p.Id==id);
+            if(p==null) return NotFound();
+
+            return Ok(p);
+        }
+
+        [HttpGet]
+        [Route("all")]
+        public IActionResult GetAll ()
+        {
+            return StatusCode(200, products.ToList());
+        }
+
+
+
+
+    }
+}
